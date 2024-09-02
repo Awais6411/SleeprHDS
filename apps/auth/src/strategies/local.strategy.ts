f83d13e4709c9f08e11b5ffc6fd5284157622e30
Awais64 <abdulhadi641164@gmail.com>
2024-09-02 14:48:17 +0500

    Apps are attested.

--- apps/auth/src/strategies/local.strategy.ts
+++ apps/auth/src/strategies/local.strategy.ts
@@ -12,7 +12,6 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     try {
       return await this.userService.varifyUser(email, password);
     } catch (error) {
-      console.log('Error');
       throw new UnauthorizedException(error);
     }
   }
--- apps/notifications/src/notifications.service.ts
+++ apps/notifications/src/notifications.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { NotifyEmailDto } from './dto/email-notify.dto';
-
+import * as nodemailer from 'nodemailer';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class NotificationsService {
-  notifyEMail(data: NotifyEmailDto) {
-    console.log({ 'NOTIFICATIONS-TESTING:=> ': data });
+  constructor(private readonly configService: ConfigService) {}
+  private readonly transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: this.configService.get('SMTP_USER'),
+      clientId: this.configService.get('GOOGLE_OAUTH_CLIENT_ID'),
+      clientSecret: this.configService.get('GOOGLE_OAUTH_CLIENT_SECRET'),
+      refreshToken: this.configService.get('GOOGLE_OAUTH_REFRESH_TOKEN'),
+    },
+  });
+  async notifyEMail(data: NotifyEmailDto) {
+    const { email } = data;
+    await this.transporter.sendMail({
+      from: this.configService.get('SMTP_USER'),
+      to: email,
+      subject: 'SLEEPR NOTIFICATIONS',
+      text: 'Congrats! Your stripe checkout payment link created successfully.',
+    });
+    console.log({ 'NOTIFICATIONS-TESTING:=> ': email });
   }
   getHello(): string {
     return 'Hello World!';
